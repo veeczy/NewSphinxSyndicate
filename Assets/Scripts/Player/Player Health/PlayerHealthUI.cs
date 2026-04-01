f8a84c29643f443e1c54b3a1f2ca6762ea74b80b
DylanJaBr <dylan1br2@gmail.com>
2026-04-01 14:16:02 -0400

    Water
    Added donut and fixed health again. found a scene bug :( Started water code

--- Assets/Scripts/Player/Player Health/PlayerHealthUI.cs
+++ Assets/Scripts/Player/Player Health/PlayerHealthUI.cs
@@ -47,9 +47,9 @@ public class PlayerHealthUI : MonoBehaviour
     {
         hearts = new Image[3]; 
 
-        hearts[0] = GameObject.Find("Heart1")?.GetComponent<Image>();
-        hearts[1] = GameObject.Find("Heart2")?.GetComponent<Image>();
-        hearts[2] = GameObject.Find("Heart3")?.GetComponent<Image>();
+        hearts[0] = GameObject.Find("Heart")?.GetComponent<Image>();
+        hearts[1] = GameObject.Find("Heart (1)")?.GetComponent<Image>();
+        hearts[2] = GameObject.Find("Heart (2)")?.GetComponent<Image>();
     }
 
     void Update()
