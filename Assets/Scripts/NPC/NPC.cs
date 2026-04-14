dc25a3134597a137f0c78d1664ce3c92953deb4b
mask789321 <ianmarcogines@gmail.com>
2026-04-14 14:45:55 -0400

    Playing with npc code

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,6 +1,40 @@
 using UnityEngine;
+
 public class NPC : MonoBehaviour
 {
     [TextArea]
     public string[] dialogueLines; // GO TO INSPECTOR & FILL IN DIALOGUE LINES!
+
+    private Animator anim;
+    private bool has Spoken = false;
+
+    void Start()
+    {
+        anim = GetComponent<animator>();
+    }
+
+    private void OnTriggerEnter2d(Collider2d collision)//box trigger for dialogue
+    {
+        if (collision.CompareTag("Player ") && !hasSpoken)
+        {
+            has spoken = true;//prevent re triggering dialogue
+            StartDialogue();
+        }
+    }
+
+    private void StartDialogue()
+    {
+        anim.SetBool("IsTalking", true);
+
+        Debug.Log("speaking!!!"); //debug remove
+    }
+
+    public void FinishDialogue()
+    {
+        anim.SetBool("IsTalking", false);
+
+        Debug.Log("going to work"); //debug remove
+    }
+
+
 }
