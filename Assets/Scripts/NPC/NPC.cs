d3591ce5e1254ffdc7f3d1e25b561189ddc8fe29
mask789321 <ianmarcogines@gmail.com>
2026-04-14 14:55:39 -0400

    fix script compiler error

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -6,18 +6,18 @@ public class NPC : MonoBehaviour
     public string[] dialogueLines; // GO TO INSPECTOR & FILL IN DIALOGUE LINES!
 
     private Animator anim;
-    private bool has Spoken = false;
+    private bool hasSpoken = false;
 
     void Start()
     {
-        anim = GetComponent<animator>();
+        anim = GetComponent<Animator>();
     }
 
-    private void OnTriggerEnter2d(Collider2d collision)//box trigger for dialogue
+    private void OnTriggerEnter2D(Collider2D collision)//box trigger for dialogue
     {
-        if (collision.CompareTag("Player ") && !hasSpoken)
+        if (collision.CompareTag("Player") && !hasSpoken)
         {
-            has spoken = true;//prevent re triggering dialogue
+            hasSpoken = true;//prevent re triggering dialogue
             StartDialogue();
         }
     }
