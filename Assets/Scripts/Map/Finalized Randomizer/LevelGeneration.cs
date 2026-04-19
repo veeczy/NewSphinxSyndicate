a3fdd5273da4b95bb1c9fc49d73763d58b5766cf
Vee Czyscon <ma191433@ucf.edu>
2026-04-18 21:04:53 -0400

    Update

--- Assets/Scripts/Map/Finalized Randomizer/LevelGeneration.cs
+++ Assets/Scripts/Map/Finalized Randomizer/LevelGeneration.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.LightTransport.PostProcessing;
 
 public class LevelGeneration : MonoBehaviour
 {
@@ -21,7 +22,7 @@ public class LevelGeneration : MonoBehaviour
         {
             numberOfRooms = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.y * 2));
         }
-        gridSizeX = Mathf.RoundToInt((worldSize.x); // the space of the world x value is set to grid size x
+        gridSizeX = Mathf.RoundToInt((worldSize.x)); // the space of the world x value is set to grid size x
         gridSizeY = Mathf.RoundToInt((worldSize.y)); // the space of the world y value is set to grid size y
         CreateRooms();
         SetRoomDoors();
@@ -72,8 +73,8 @@ public class LevelGeneration : MonoBehaviour
             int index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1)); // get index
             x = (int)takenPositions[index].x; // get x value
             y = (int)takenPositions[index].y; // get y value
-            bool UpDown = (Random.value < 0.5f); // randomly decide if moving vertical/horizontal
-            bool positive = (Random.value < 0.5f); // randomly decide if positive/negative
+            bool UpDown = (Random.value < 0.5f); // randomly decide if moving vertical/horizontal (up/down)
+            bool positive = (Random.value < 0.5f); // randomly decide if positive/negative (left/right)
 
             if (UpDown) // if moving vertical
             {
@@ -88,5 +89,99 @@ public class LevelGeneration : MonoBehaviour
 
         } while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
         return checkingPos;
+    }
+
+    Vector2 SelectiveNewPosition() 
+    {
+        // reset data
+        int index = 0, inc = 0;
+        int x = 0, y = 0;
+        Vector2 checkingPos = Vector2.zero;
+
+        do // modified from new room to find rooms with only one neighbor position
+        {
+            inc = 0;
+            do { index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1)); inc++; } while (NumberOfNeighbors(takenPositions[index], takenPositions) > 1 && inc < 100); // get index of room when neighbors greater than 1
+            x = (int) takenPositions[index].x; // get x value
+            y = (int) takenPositions[index].y; // get y value
+            bool UpDown = (Random.value < 0.5f); // randomly decide if moving vertical/horizontal (up/down)
+            bool positive = (Random.value < 0.5f); // randomly decide if positive/negative (left/right)
+
+            if (UpDown) // if moving vertical
+            {
+                if (positive) { y += 1; }
+                else { y -= 1; }
+            }
+            else // if moving horizontalright 
+            {
+                if (positive) { x += 1; }
+                else { x -= 1; }
+            }
+
+            checkingPos = new Vector2(x, y);
+        } while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
+
+        if(inc >= 100) { Debug.Log("error: could not find position with only one neighbor!"); }
+
+        return checkingPos;
+    }
+    int NumberOfNeighbors(Vector2 checkingPos, List<Vector2> usedPositions)
+    {
+        int ret = 0; // reset value every time you check
+
+        //increment it for each taken position that is fulfilled - ie if one spot if filled then it is 1, if all fouer sps filled then it is 4
+        if (usedPositions.Contains(checkingPos + Vector2.right)) { ret++; }
+        if (usedPositions.Contains(checkingPos + Vector2.left)) { ret++; }
+        if (usedPositions.Contains(checkingPos + Vector2.up)) { ret++; }
+        if (usedPositions.Contains(checkingPos + Vector2.down)) { ret++; }
+
+        return ret; // return the value so you know how many neighbors are around the position
+    }
+
+    void SetRoomDoors() // find where rooms around are located from current position
+    {
+        for (int x = 0; x < ((gridSizeX * 2)); x++) // checks every x position in array
+        {
+            for(int y = 0; y < ((gridSizeY * 2)); y++) // checks every y position in array (as a coordinate of (x,y) now)
+            {
+                if (rooms[x,y] == null) { continue; } // if there is nothing there, go to next position of the array
+
+                Vector2 gridPosition = new Vector2(x, y); // check to see if there is a room in each cardinal direction
+                
+                if (y - 1 < 0) { rooms[x, y].doorBottom = false; } // check above
+                else { rooms[x,y].doorBottom = (rooms[x, y - 1] != null); }
+
+                if (y + 1 >= gridSizeY * 2) { rooms[x,y].doorTop = false; } // check bellow
+                else { rooms[x, y].doorTop = (rooms[x, y + 1] != null); }
+
+                if (x - 1 < 0) { rooms[x,y].doorLeft  = false; } // check left
+                else { rooms[x, y].doorLeft = (rooms[x - 1, y] != null); }
+
+                if (x + 1 >= gridSizeX * 2) { rooms[x,y].doorRight = false; } // check right
+                else { rooms[x, y].doorRight = (rooms[x + 1, y] != null); }
+            }
+        }
+    }
+
+    void DrawMap()
+    {
+        foreach (RoomType room in rooms) // loop for every coordinate
+        {
+            if (room == null) { continue; } //if there isnt a room in that position on the map, skip to next slot
+
+            Vector2 drawPos = room.gridPos; // gather coordinate
+
+            // multiply to size of map sprite px
+            drawPos.x *= 16;
+            drawPos.y *= 16;
+
+            // draw based on info in Map Sprite Selector script
+            MapSpriteSelector mapper = Object.Instantiate(roomWhiteObj, drawPos, Quaternion.identity).GetComponent<MapSpriteSelector>();
+            mapper.type = room.type;
+            mapper.up = room.doorTop;
+            mapper.down = room.doorBottom;
+            mapper.right = room.doorRight;
+            mapper.left = room.doorLeft;
+        }
     }
 }
---
+++ Assets/Scripts/Map/Finalized Randomizer/MapSpriteSelector.cs
@@ -0,0 +1,162 @@
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class MapSpriteSelector : MonoBehaviour
+{
+    [Header("Sprites")]
+    public Sprite[] MapSprites;
+    //basic rooms [type 1]
+    public Sprite spriteUp, spriteDown, spriteRight, spriteLeft, spriteUpDown; // one ways
+    public Sprite spriteLeftRight, spriteUpRight, spriteUpLeft, spriteDownRight, spriteDownLeft; // two ways
+    public Sprite spriteUpDownLeft, spriteUpLeftRight, spriteUpDownRight, spriteDownLeftRight; // three ways
+    public Sprite spriteUpDownLeftRight; // four ways
+
+    //special rooms
+    public Sprite spriteUpTreasure, spriteDownTreasure, spriteRightTreasure, spriteLeftTreasure; // treasure rooms (basic one ways) [type 3]
+    public Sprite spriteUpBoss, spriteDownBoss, spriteRightBoss, spriteLeftBoss; // boss rooms [type 2]
+
+    //alt rooms
+    //public Sprite spriteUpDownAlt, spriteUpDownLeftRightAlt; // desert alt rooms
+    //public Sprite spriteDownRightAlt, spriteUpDownLeftAlt, spriteUpDownRightAlt, spriteUpLeftAlt, spriteUpLeftRightAlt; // city alt rooms 
+    //public Sprite spriteLeftRightAlt, spriteUpRightAlt; // swamp alt rooms
+
+    public Sprite spriteTreasureIcon, spriteBossIcon, spriteStartIcon;
+
+    [Header("Map Assign Data")]
+    public int type;
+    public bool up, down, left, right = false;
+    // room type 0 - entry/starting room
+    // room type 1 - normal room
+    // room type 2 - boss room 
+    // room type 3 - special room (for when we create caves? or possibly for special rooms?)
+
+    public bool treasure, boss = false;
+
+    [Header("Map Render Data")]
+    public Color mainColor;
+    public Color normalColor, enterColor;
+    SpriteRenderer rend;
+
+    //[Header("Minimap Data")]
+    //public GameObject ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8; // 1st row
+    //public GameObject bx1, bx2, bx3, bx4, bx5, bx6, bx7, bx8; // 2nd row
+    //public GameObject cx1, cx2, cx3, cx4, cx5, cx6, cx7, cx8; // 3rd row
+    //public GameObject dx1, dx2, dx3, dx4, dx5, dx6, dx7, dx8; // 4th row
+    //public GameObject ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8; // 5th row
+    //public GameObject fx1, fx2, fx3, fx4, fx5, fx6, fx7, fx8; // 6th row
+    //public GameObject gx1, gx2, gx3, gx4, gx5, gx6, gx7, gx8; // 7th row
+    //public GameObject hx1, hx2, hx3, hx4, hx5, hx6, hx7, hx8; // 8th row
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InitializeSprites();
+        //InitializeGameObjects();
+
+        //temp
+        rend = GetComponent<SpriteRenderer>();
+        mainColor = normalColor;
+        PickSprite();
+        PickColor();
+    }
+
+    public void InitializeGameObjects()
+    {
+        //how to get the different gameobjects for the map
+    }
+    public void InitializeSprites()
+    {
+        // one ways
+        spriteUp = MapSprites[0];
+        spriteDown = MapSprites[1];
+        spriteLeft = MapSprites[2];
+        spriteRight = MapSprites[3];
+
+        // two ways
+        spriteUpDown = MapSprites[4];
+        spriteLeftRight = MapSprites[5];
+
+        spriteUpRight = MapSprites[6];
+        spriteUpLeft = MapSprites[7];
+
+        spriteDownRight = MapSprites[8];
+        spriteDownLeft = MapSprites[9];
+
+        // three ways
+        spriteUpDownLeft = MapSprites[10];
+        spriteUpDownRight = MapSprites[11];
+
+        spriteDownLeftRight = MapSprites[12];
+        spriteUpLeftRight = MapSprites[13];
+
+        // four ways
+        spriteUpDownLeftRight = MapSprites[14];
+
+        // treasure rooms
+        spriteUpTreasure = MapSprites[15];
+        spriteDownTreasure = MapSprites[16];
+        spriteLeftTreasure = MapSprites[17];
+        spriteRightTreasure = MapSprites[18];
+
+        // boss rooms
+        spriteUpBoss = MapSprites[19];
+        spriteDownBoss = MapSprites[20];
+        spriteLeftBoss = MapSprites[21];
+        spriteRightBoss = MapSprites[22];
+
+        // alt rooms
+
+
+        //treausre, start, boss icons
+        //spriteStartIcon = MapSprites[i];
+        //spriteTreasureIcon = MapSprites[i];
+        //spriteBossIcon = MapSprites[i];
+    }
+
+    void PickSprite()
+    {
+        if(type == 0 || type == 1) { treasure = false; boss = false; }
+        if(type == 2) { treasure = false; boss = true; }
+        if(type == 3) { treasure = true; boss = false; }
+
+        //one ways
+        if (up && !down && !left && !right) { if(treasure) { rend.sprite = spriteUpTreasure; } if(boss) { rend.sprite = spriteUpBoss; } else { rend.sprite = spriteUp; } }
+        if (down && !up && !left && !right) { if(treasure) { rend.sprite = spriteDownTreasure; } if(boss) { rend.sprite = spriteDownBoss; } else { rend.sprite = spriteDown; } }
+        if (left && !down && !up && !right) { if (treasure) { rend.sprite = spriteLeftTreasure; } if (boss) { rend.sprite = spriteLeftBoss; } else { rend.sprite = spriteLeft; } }
+        if (right && !down && !left && !up) { if (treasure) { rend.sprite = spriteRightTreasure; } if (boss) { rend.sprite = spriteRightBoss; } else { rend.sprite = spriteRight; } }
+
+        //two ways
+        if (up && down && !left && !right) { rend.sprite = spriteUpDown; }
+        if (!up && !down && left && right) { rend.sprite = spriteLeftRight; }
+
+        if (up && !down && !left && right) { rend.sprite = spriteUpRight; }
+        if (up && !down && left && !right) { rend.sprite = spriteUpLeft; }
+
+        if (!up && down && !left && right) { rend.sprite = spriteDownRight; }
+        if (!up && down && left && !right) { rend.sprite = spriteDownLeft; }
+
+        //three ways
+        if (up && down && !left && right) { rend.sprite = spriteUpDownRight; }
+        if (up && down && left && !right) { rend.sprite = spriteUpDownLeft; }
+
+        if (!up && down && left && right) { rend.sprite = spriteDownLeftRight; }
+        if (up && !down && left && right) { rend.sprite = spriteUpLeftRight; }
+
+        //four ways
+        if (up && down && left && right) { rend.sprite = spriteUpDownLeftRight; }
+    }
+
+    void PickColor()
+    {
+        // choose the color of the room on the map
+        if(type == 0) { mainColor = enterColor; }
+        if(type == 1) { mainColor = normalColor; } // normal room
+
+        //this is where id put if it is the room arnolds in (for minimap) to change color to be darker
+
+        // . . .
+
+        rend.color = mainColor; // render the color
+    }
+}
