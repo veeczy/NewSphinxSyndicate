2364a801d4ada8e35266e3285cc7658d2cd9bb29
Vee Czyscon <ma191433@ucf.edu>
2026-04-19 21:55:10 -0400

    BlackJack Credits Fix & EnemyIcon & City Credits Fix

--- Assets/Scripts/EnemyCounter.cs
+++ Assets/Scripts/EnemyCounter.cs
@@ -6,20 +6,11 @@ public class EnemyCounterUI : MonoBehaviour
     [Header("UI Reference")]
     public TMP_Text enemyText;
     public TMP_Text enemyShadow;
+    public GameObject enemyIcon;
 
     private void Start()
     {
-        //retrieve ui if not linked in inspector
-        if (enemyText == null) // enemy counter text
-        {
-            GameObject placeholder = GameObject.Find("EnemyCounterTxt");
-            enemyText = placeholder.GetComponent<TMP_Text>();
-        }
-        if (enemyShadow == null) // enemy counter text shadow
-        {
-            GameObject placeholder = GameObject.Find("EnemyCounterTxtShadow");
-            enemyShadow = placeholder.GetComponent<TMP_Text>();
-        }
+        InitializeGameObjects();
     }
     void Update()
     {
@@ -41,13 +32,44 @@ public class EnemyCounterUI : MonoBehaviour
             {
                 enemyText.text = aliveCount.ToString();
                 enemyShadow.text = enemyText.text;
+                enemyIcon.SetActive(true);
             }
             if(aliveCount <= 0) //hide ui if no enemies in area
             {
                 enemyText.text = "";
+                enemyIcon.SetActive(false);
                 enemyShadow.text = enemyText.text;
             }
             
         }
+    }
+
+    private GameObject FindInactiveObjectByName(string name)
+    {
+        GameObject[] objects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+        foreach (GameObject obj in objects)
+        {
+            if (obj.name == name && obj.scene.isLoaded)
+                return obj;
+        }
+
+        return null;
+    }
+
+    public void InitializeGameObjects()
+    {
+        //retrieve ui if not linked in inspector
+        if (enemyText == null) // enemy counter text
+        {
+            GameObject placeholder = GameObject.Find("EnemyCounterTxt");
+            enemyText = placeholder.GetComponent<TMP_Text>();
+        }
+        if (enemyShadow == null) // enemy counter text shadow
+        {
+            GameObject placeholder = GameObject.Find("EnemyCounterTxtShadow");
+            enemyShadow = placeholder.GetComponent<TMP_Text>();
+        }
+        if (enemyIcon == null) { enemyIcon = GameObject.Find("EnemyIcon"); enemyIcon = FindInactiveObjectByName("EnemyIcon"); }
     }
 }
