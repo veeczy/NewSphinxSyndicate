65a8ebd76e2689336c7548c85beb4a91fbed9bb7
mask789321 <ianmarcogines@gmail.com>
2026-04-26 16:41:11 -0400

    1

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+
+{
+    [Header("----------Audio Source----------")]
+    [SerializeField] AudioSource musicSource;
+
+    [Header("----------Audio Clip------------")]
+    public AudioClip background;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+}
